0d50cedef08f29a19d03696766a0f26026c83783
小浩 <sdwenhappy@163.com>
2022-02-12 13:15:31 +0800

    Update deploy-server.sh

--- deploy-server.sh
+++ deploy-server.sh
@@ -4,6 +4,7 @@
 local_path="{local_path}"
 remote_path="{remote_path}"
 remote_user_host="{remote_user_host}"
+app_port="{app_port}"
 
 echo '> 开始打包'
 cd $local_path || exit
@@ -13,16 +14,11 @@ echo '> 开始上传'
 scp $local_path/build/libs/*.jar $remote_user_host:$remote_path
 
 echo '> 开始启动'
-ssh -Tq $remote_user_host << "EOF"
-
-# 配置启动端口和路径
-app_port="{app_port}"
-remote_path="{remote_path}"
-
+ssh -Tq $remote_user_host <<EOF
 cd $remote_path
 pid=$(lsof -i:$app_port | sed -n "2,1p" | awk '{print $2}')
-test -n "$pid" && kill -9 "$pid"
-nohup java -jar *.jar >nohup.out 2>&1 &
+test -n "\$pid" && kill -9 "\$pid"
+nohup java -jar *.jar > nohup.out 2>&1 &
 exit
 EOF
 
