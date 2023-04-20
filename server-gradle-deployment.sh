bd609198e9c97360b0e519bfe1a6e52d77ddf9e8
小浩 <sdwenhappy@163.com>
2023-04-21 02:27:57 +0800

    feat: 更新部分配置

--- server-gradle-deployment.sh
+++ server-gradle-deployment.sh
@@ -8,7 +8,7 @@ server_port="8656"
 
 echo '> 开始打包'
 cd "$project_path" || exit
-bash gradlew bootJar || exit
+./gradlew bootJar || exit
 
 echo '> 开始上传'
 ssh -Tq "$server_host" "mkdir -p $server_directory"
