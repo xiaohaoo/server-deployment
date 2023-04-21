388055863b6d3cf25b3d6eb88585a0bc3834efdf
小浩 <sdwenhappy@163.com>
2023-04-21 16:15:23 +0800

    feat: 更新部分配置

--- server-gradle-deployment.sh
+++ server-gradle-deployment.sh
@@ -9,8 +9,6 @@ server_port=${3}
 
 
 echo '> 开始上传'
-ssh -Tq "$server_host" "mkdir -p $server_directory"
-scp "$project_path"/build/libs/*.jar "$server_host":"$server_directory"
 
 echo '> 开始启动'
 ssh -Tq "$server_host" <<EOF
