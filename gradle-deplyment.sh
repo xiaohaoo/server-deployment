826c896adec12c5bbf21f10a017bbe4ecc360f23
小浩 <sdwenhappy@163.com>
2023-05-08 16:01:44 +0800

    feat: 更新部分配置

--- gradle-deplyment.sh
+++ gradle-deplyment.sh
@@ -10,7 +10,6 @@ echo '> 开始打包'
 cd "$project_path" || exit
 
 echo '> 开始启动'
-ssh -Tq "$server_host" "mkdir -p $server_directory"
 scp "$project_path"/build/libs/*.jar "$server_host":"$server_directory"
 
 echo '> 开始上传'
